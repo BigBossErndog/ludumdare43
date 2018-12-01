3eff1561218ffe9ca20cbb868b840c9b8fe080d7
eagp <eagp@pc3-019-l.cs.st-andrews.ac.uk>
2018-12-01 13:38:58 +0000

    Basic Gun and Shield

--- game/js/weapons.js
+++ game/js/weapons.js
@@ -1,5 +1,6 @@
 function loadWeapons() {
 	game.load.spritesheet("bullet1", "assets/bullet1.png");
+	game.load.image("shield1", "assets/shield1.png");
 }
 
 function basicGun(owner) {
@@ -8,3 +9,17 @@ function basicGun(owner) {
 
 	return newgun;
 }
+
+function shield(owner) {
+	var shield = game.add.sprite();
+	shield.shieldOwner = owner;
+	shield.shieldLogic = function() {
+		this.angle += 1;
+		this.x = this.shieldOwner.x + Math.sin(this.angle) * 50;
+		this.y = this.shieldOwner.y + Math.cos(this.angle) * 50;
+	}
+
+	return shield;
+}
+
+class LaserGun() 
