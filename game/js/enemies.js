2a65a98f97de9ea12b3d0e786b4c9718402f5db8
eagp <eagp@pc3-019-l.cs.st-andrews.ac.uk>
2018-12-01 15:22:20 +0000

    Updates to human

--- game/js/enemies.js
+++ game/js/enemies.js
@@ -6,6 +6,7 @@ function makeHuman(x, y) {
 	var human = game.add.sprite(x, y, "human");
 	human.anchor.x = 0.5;
 	human.anchor.y = 0.5;
+	game.physics.enable(human, Phaser.Physics.ARCADE);
 
 	human.logic = function() {
 		this.angle = Math.atan2(player.head.body.y - this.body.y, player.head.body.x - this.body.x) * (180/Math.PI);
@@ -13,6 +14,8 @@ function makeHuman(x, y) {
 		this.body.velocity.x = Math.sin(this.angle);
 		this.body.velocity.y = Math.cos(this.angle);
 	}
+
+	return human;
 }
 
 function makeDefaultEnemy(x, y) {
