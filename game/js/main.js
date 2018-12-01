f7170f98cbdfe97c0e7dc0c28bb1694c9fc30224
eagp <eagp@pc3-019-l.cs.st-andrews.ac.uk>
2018-12-01 20:22:07 +0000

    Common group for humans and enemies implemented

--- game/js/main.js
+++ game/js/main.js
@@ -9,7 +9,7 @@ window.onload = function() {
 function preload() {
     console.log("preload");
     game.load.spritesheet('head', 'assets/Head.png', 32, 32);
-    game.load.spritesheet('legs', 'assets/Legs.png', 32, 32);
+    game.load.spritesheet('legs', 'assets/WalkSprite.png', 32, 32);
     game.load.spritesheet('target', 'assets/target.jpg');
 	game.load.image("playercom", "assets/playercom.png");
     loadWeapons();
@@ -42,12 +42,12 @@ function create() {
 	map = makeLevel("mapTest1", "spritemap2");
 
     //  This will check Group vs. Group collision (bullets vs. veggies!)
-    humans = game.add.group();
+    aigroup = game.add.group();
 	var x, y;
     for (var i = 0; i < 5; i++) {
      	x = getRandomInt(0, 800);
 		y = getRandomInt(0, 600);
-        humans.add(makeHuman(x, y));
+        aigroup.add(makeDefaultEnemy(x, y));
     }
 
 	x = getRandomInt(300, 500);
@@ -57,6 +57,10 @@ function create() {
     player.com.addChild(player.head = game.add.sprite(0, 0, 'head'));
     targeter = game.add.sprite(0, 0, 'target');
     gun = smg(player.head);
+
+	player.legs.animations.add("walk", [0,1,2,3,4,5,6,7,8,9,10,11,12,13], 20, true);
+	player.legs.animations.add("stand", [0], 1, false);
+	player.legs.animations.play("stand");
 
     targeter.anchor.x = 0.5;
     targeter.anchor.y = 0.5;
@@ -105,13 +109,13 @@ var nextFire = 0;
 
 function update() {
 
-	game.physics.arcade.overlap(gun.bullets, humans, collisionHandler, null, this);
+	game.physics.arcade.overlap(gun.bullets, aigroup, collisionHandler, null, this);
 	game.physics.arcade.collide(gun.bullets, map.wallLayer, function(bullet) {
 		bullet.kill();
 	});
 
-    for (var i = 0; i < humans.length; i++) {
-        humans.getAt(i).logic();
+    for (var i = 0; i < aigroup.length; i++) {
+        aigroup.getAt(i).logic();
     }
 
     targeter.x = game.input.mousePointer.x + game.camera.x;
@@ -126,22 +130,30 @@ function update() {
     if (cursors.left.isDown || this.wasd.left.isDown)
     {
         player.com.body.velocity.x += -240;
+		player.legs.animations.play("walk");
     }
      if (cursors.right.isDown || this.wasd.right.isDown)
     {
         player.com.body.velocity.x += 240;
+		player.legs.animations.play("walk");
     }
     if (cursors.up.isDown || this.wasd.up.isDown) {
         player.com.body.velocity.y += -240;
+		player.legs.animations.play("walk");
     }
     if (cursors.down.isDown || this.wasd.down.isDown) {
         player.com.body.velocity.y += 240;
+		player.legs.animations.play("walk");
     }
 
     if (player.com.body.velocity.y != player.com.body.velocity.x || player.com.body.velocity.x != 0) {
         angle = Math.atan2(player.com.body.velocity.y, player.com.body.velocity.x) * (180/Math.PI);
         player.legs.angle = (angle + previousAngle + previousPreviousAngle) / 3;
     }
+	else {
+		player.legs.animations.play("stand");
+	}
+
     previousAngle = angle;
     previousPreviousAngle = previousAngle;
 
@@ -153,14 +165,14 @@ function update() {
 	// player.head.rotation = game.physics.arcade.angleToPointer(player.head);
 	player.head.angle = Math.atan2((game.input.mousePointer.y + game.camera.y) - player.com.body.y, (game.input.mousePointer.x + game.camera.x) - player.com.body.x) * (180/Math.PI);
 
-	game.physics.arcade.collide(humans);
-	game.physics.arcade.collide(humans, player.com);
+	game.physics.arcade.collide(aigroup);
+	game.physics.arcade.collide(aigroup, player.com);
 
-	game.physics.arcade.collide(humans, map.wallLayer);
+	game.physics.arcade.collide(aigroup, map.wallLayer);
 	game.physics.arcade.collide(player.com, map.wallLayer);
 }
 
-function collisionHandler(bullet, human) {
+function collisionHandler(bullet, ai) {
 	bullet.kill();
-	human.kill();
+	ai.kill();
 }
