8c2fae1ecc23193584d83069e98330cb018bf123
ikl <ikl@pc3-042-l.cs.st-andrews.ac.uk>
2018-12-02 18:44:26 +0000

    Fixed the fireLimit bug (requires weird increment-decrement workaround). Added Typhoon Weapon System, 360degree blast radius.

--- game/js/weapons.js
+++ game/js/weapons.js
@@ -1,6 +1,6 @@
 function loadWeapons() {
 	game.load.spritesheet("bullet1", "assets/bullet1.png");
-	game.load.spritesheet("railbullet", "assets/bullet1.png");
+	game.load.spritesheet("invistest", "assets/playercom.png");
 	game.load.image("shield1", "assets/shield1.png");
 }
 
@@ -11,9 +11,9 @@ function pistol(owner) {
 	newgun.bullets.setAll('scale.y', 0.25);
 	newgun.fireRate = 500;
 	newgun.fireRateVariance = 200;
-	newgun.bulletSpeed = 350;
-	newgun.bulletSpeedVariance = 150;
-	newgun.bulletAngleVariance = 20;
+	newgun.bulletSpeed = 450;
+	newgun.bulletSpeedVariance = 50;
+	newgun.bulletAngleVariance = 1;
 	newgun.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
 	newgun.fireLimit = 8;
 	newgun.weaponName = "Basic Gun";
@@ -43,7 +43,6 @@ function megaParticleCannon(owner) {
 	if (owner.head !== undefined) newgun.trackSprite(owner.head, 25, 5, true);
 	else newgun.trackSprite(owner, 75, 5, true);
 	newgun.bullets.setAll('scale.x', 3);
-	newgun.bullets.setAll('scale.y', 10);
 	newgun.fireRate = 0.5;
 	// newgun.fireRateVariance = 1;
 	newgun.bulletSpeed = 1000;
@@ -52,20 +51,24 @@ function megaParticleCannon(owner) {
 	newgun.multiFire = true;
 	newgun.specialFiring = false;
 	newgun.shoot = function () {
+		console.log(newgun.fireLimit > newgun.shots);
 		if (!newgun.specialFiring && newgun.fireLimit > newgun.shots) {
 			console.log("fire start");
 			newgun.specialFiring = true;
-			owner.com.immovable = true;
+			// owner.com.immovable = true;
 			//lock in place - deregister inputs?
 			console.log(game);
 			//start timer
+			var scale = 1;
 			var fireLoop = game.time.events.loop(50, function() {
 				newgun.fire();
-			}, this, newgun);
+				newgun.bullets.setAll('scale.y', scale += 0.15);
+			}, this, newgun, scale);
 			game.time.events.add(Phaser.Timer.SECOND * 2, function() {
 				newgun.specialFiring = false;
-				owner.com.immovable = false;
-				game.time.events.stop(fireLoop);
+				// owner.com.immovable = false;
+				game.time.events.remove(fireLoop);
+				newgun.bullets.setAll('scale.y', 1);
 				console.log("fire over");
 			}, this, newgun, fireLoop, owner);
 			//spawn blast
@@ -113,6 +116,7 @@ function shotgun(owner) {
 	shotgun.multiFire = true;
 	shotgun.weaponName = "Shot Gun";
 	shotgun.shoot = function () {
+		if (shotgun.shots === 5) shotgun.fireLimit = 7;
 		var success = false;
 
 		//insert check for enough bullets to fire full spread
@@ -121,6 +125,7 @@ function shotgun(owner) {
 				success = true;
 			}
 		}
+		if (shotgun.shots === 6) shotgun.fireLimit = 6;
 
 		return success;
 	}
@@ -155,10 +160,52 @@ function defaultMelee(owner) {
 	// punch.bulletAngleVariance = 10;
 	punch.bulletKillType = Phaser.Weapon.KILL_LIFESPAN;
 	punch.bulletLifespan = 5;
-	punch.weaponName = "melee";
+	punch.type = "melee";
 	punch.weaponName = /*FALCON*/"Punch";
 
 	return punch;
+}
+
+function typhoon(owner) {
+	var typhoon = game.add.weapon(36, "bullet1");
+	typhoon.type = 'special';
+	if (owner.head !== undefined) typhoon.trackSprite(owner.head, 0, 0, true);
+	else typhoon.trackSprite(owner, 15, 5, true);
+	typhoon.fireRate = 480;
+	typhoon.bulletSpeed = 500;
+	typhoon.bulletAngleVariance = 360;
+	typhoon.fireLimit = 1;
+	typhoon.bulletKillType = Phaser.Weapon.KILL_DISTANCE;
+	typhoon.bulletKillDistance = 256;
+	typhoon.multiFire = true;
+	typhoon.autoExpandBulletsGroup  = true;
+	typhoon.shoot = function () {
+		if (typhoon.shots >= 1) return;
+		game.time.slowMotion = 5.0;
+		game.time.desiredFps = 300;
+		var success = false;
+
+		//insert check for enough bullets to fire full spread
+
+		game.time.events.add(Phaser.Timer.SECOND * 1, function() {
+			typhoon.fireLimit = 2;
+			for (var i = 0; i < 60; i++) {
+				if (typhoon.fire()) {
+					success = true;
+				}
+			}
+			typhoon.fireLimit = 1;
+		}, this, typhoon);
+		game.time.events.add(Phaser.Timer.SECOND * 2, function() {
+			typhoon.specialFiring = false;
+			game.time.slowMotion = 1;
+			game.time.desiredFps = 60;
+		}, this, typhoon);
+
+		return success;
+	}
+
+	return typhoon;
 }
 
 function shield(owner) {
