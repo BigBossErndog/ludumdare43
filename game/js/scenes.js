43482963bf1cd14ef460d8d2e52e50d88c8b7dcb
Ernest Placido <manage.bigbosserndog@gmail.com>
2018-12-02 21:38:16 +0000

    Parallax, plus pickables done

--- game/js/scenes.js
+++ game/js/scenes.js
@@ -2,22 +2,31 @@ var test;
 
 var mainScene = {
 	preload: function() {
-		game.load.spritesheet('reticle', 'assets/reticle.png', 15, 15);
-		game.load.image("parallax", "assets/Parallax.png");
-		loadWeapons();
-		loadEnemies();
-		loadLevels();
-		loadPlayer();
+		game.load.tilemap("Level1", "assets/tilemaps/Level1.json", null, Phaser.Tilemap.TILED_JSON);
+		loadDefaults();
 	},
 	
 	create: function() {
 		createControls();
 		parallaxSprite = game.add.sprite(0, 0, "parallax");
 		parallaxSprite.fixedToCamera = true;
+		parallaxSprite.anchor.x = 0.5;
+		parallaxSprite.anchor.y = 0.5;
 	
-		map = makeLevel("maptest1", "Sprite Map 2", "spritemap2");
+		map = makeLevel("Level1", "Sprite Map 2", "spritemap2");
 		
 		createDefaults();
+		
+		var spawnPoints = [ [30,40], [60,70], [100, 50], [550, 370], [190, 500] ];
+		for (var i = 0; i < 5; i++) {
+			let newEnemy = makeDefaultEnemy(spawnPoints[i][0], spawnPoints[i][1]);
+			newEnemy.gun = autorifle(newEnemy);
+			aigroup.add(newEnemy);
+		}
+		
+		for (var i = 0; i < 10; i++) {
+			let newPickable = shotgunPickable(Math.random() * map.widthInPixels, Math.random() * map.heightInPixels);
+		}
 	},
 	
 	update: function() {
