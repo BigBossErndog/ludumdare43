d9f6fa7e8de5602b8d2f2d3dd33e575587e70356
ikl <ikl@pc3-042-l.cs.st-andrews.ac.uk>
2018-12-02 20:12:11 +0000

    Beginnings of quest/dialogue system.

---
+++ game/js/questHandler.js
@@ -0,0 +1,16 @@
+function triggerQuest(trigger, player) {
+
+}
+
+function triggerDialogue(trigger, player) {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        console.log("it worked");
+        // Great success! All the File APIs are supported.
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
+}
+
+function triggerSceneChange(trigger, player, game) {
+
+}
