174fc89c8547cd2d4b5ab5812de1da05a221a9a0
ikl <ikl@pc3-042-l.cs.st-andrews.ac.uk>
2018-12-02 22:38:38 +0000

    Added names to pickables for when they're scanned.

--- game/js/pickables.js
+++ game/js/pickables.js
@@ -1,131 +1,134 @@
 function pistolPickable(x, y) {
 	var sprite = game.add.sprite(x, y, "pickables");
+	sprite.pickableName = "Pistol";
 	sprite.animations.add("item", [0], 1, false);
 	sprite.animations.play("item");
 	sprite.ammo = null;
 	sprite.dropped = false;
 	sprite.anchor.x = 0.5;
 	sprite.anchor.y = 0.5;
-	
+
 	game.physics.enable(sprite, Phaser.Physics.ARCADE);
-	
+
 	sprite.body.drag.x = 1000;
 	sprite.body.drag.y = 1000;
-	
+
 	sprite.logic = function() {
 		game.physics.arcade.collide(this, map.wallLayer);
 		this.dropped = false;
 	}
-	
+
 	sprite.setVelocity = function(x, y) {
 		this.body.velocity.x += x;
 		this.body.velocity.y += y;
-		
+
 		this.angle += (this.body.velocity.x + this.body.velocity.y)/1000;
 	}
-	
+
 	sprite.pickUp = function() {
 		if (player.gun != null) {
 			player.gun.destroy();
 		}
-		
+
 		player.gun = pistol(player);
 		this.destroy();
-		
+
 		player.gun.shots = this.ammo;
-		
+
 		player.playStandAnimation();
 	}
-	
+
 	pickables.add(sprite);
-	
+
 	return sprite;
 }
 
 function smgPickable(x, y) {
 	var sprite = game.add.sprite(x, y, "pickables");
+	sprite.pickableName = "SMG";
 	sprite.animations.add("item", [1], 1, false);
 	sprite.animations.play("item");
 	sprite.ammo = null;
 	sprite.dropped = false;
 	sprite.anchor.x = 0.5;
 	sprite.anchor.y = 0.5;
-	
+
 	game.physics.enable(sprite, Phaser.Physics.ARCADE);
-	
+
 	sprite.body.drag.x = 1000;
 	sprite.body.drag.y = 1000;
-	
+
 	sprite.logic = function() {
 		game.physics.arcade.collide(this, map.wallLayer);
 		this.dropped = false;
 	}
-	
+
 	sprite.setVelocity = function(x, y) {
 		this.body.velocity.x += x;
 		this.body.velocity.y += y;
-		
+
 		this.angle += (this.body.velocity.x + this.body.velocity.y)/1000;
 	}
-	
+
 	sprite.pickUp = function() {
 		if (player.gun != null) {
 			player.gun.destroy();
 		}
-		
+
 		player.gun = smg(player);
 		this.destroy();
-		
+
 		player.gun.shots = this.ammo;
-		
+
 		player.playStandAnimation();
 	}
-	
+
 	pickables.add(sprite);
-	
+
 	return sprite;
 }
 
 function shotgunPickable(x, y) {
 	var sprite = game.add.sprite(x, y, "pickables");
+	sprite.pickableName = "Shotgun";
 	sprite.animations.add("item", [2], 1, false);
 	sprite.animations.play("item");
 	sprite.ammo = null;
 	sprite.dropped = false;
 	sprite.anchor.x = 0.5;
 	sprite.anchor.y = 0.5;
-	
+
 	game.physics.enable(sprite, Phaser.Physics.ARCADE);
-	
+
 	sprite.body.drag.x = 400;
 	sprite.body.drag.y = 400;
-	
+
 	sprite.logic = function() {
 		game.physics.arcade.collide(this, map.wallLayer);
 		this.dropped = false;
-		
+
 		this.angle += Math.abs(this.body.velocity.x + this.body.velocity.y)/10;
 	}
-	
+
 	sprite.setVelocity = function(x, y) {
 		this.body.velocity.x = x;
 		this.body.velocity.y = y;
 	}
-	
+
 	sprite.pickUp = function() {
 		if (player.gun != null) {
 			player.gun.destroy();
 		}
-		
+
 		player.gun = shotgun(player);
 		this.destroy();
-		
+
 		player.gun.shots = this.ammo;
-		
+
 		player.playStandAnimation();
 	}
-	
+
 	pickables.add(sprite);
-	
+
 	return sprite;
 }
